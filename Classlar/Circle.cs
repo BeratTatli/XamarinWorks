894d57db05926cf59b4c3b78832a2097307f32a1
BeratTatli <61687407+BeratTatli@users.noreply.github.com>
2021-01-02 12:20:57 +0300

    initialize

---
+++ Classlar/Circle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classlar
+{
+    class Circle
+    {
+        //Instance variable
+        public double radius = 1;
+        public static int numberOfCircles = 0;
+
+
+        
+       public Circle(double r) {
+            radius = r;
+            double a = r;
+            //This line is local variable.
+            numberOfCircles = numberOfCircles + 1;
+
+        }
+
+        public Circle()
+        {
+            numberOfCircles = numberOfCircles + 1;
+        }
+        
+        public double getarea(int state)
+        {
+            double area = Math.PI * radius * radius;
+
+            if (state == 1)
+            {
+                return area;
+            }
+            else
+            {
+                return (int)area;
+            }
+
+            
+
+            //Eger asagıdakı gibi bir satır yazarsak ve fonksiyonu oyle bıtırırsek, local variable
+            //oldugu ıcın program.cs de calısmayacak
+
+            //double area =  Math.PI * radius * radius;
+            //return area;b 
+        }
+        public static double calculateArea(double r)
+        {
+            return Math.PI * r * r;
+        }
+        public double getPerimeter()
+        {
+            return Math.PI * 2 * radius;
+        }
+     
+
+    }
+}
---
+++ Classlar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Classlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+            Circle c2 = new Circle(2.0);
+
+            Circle c3 = new Circle();
+            Circle c4 = new Circle(3.0);
+
+
+            Circle c5 = new Circle();
+            Circle c6 = new Circle(4.0);
+            //Number of circles statik bir deger,bundan dolayı duragandır yanı etkılenmez ve Circle. yazarak istediğimiz gibi erişebiliriz.
+            Circle.numberOfCircles++;
+            Console.WriteLine("Daire sayısı statik degiskenle: {0}",Circle.numberOfCircles);
+
+            Console.WriteLine("c1 alanı : {0}",c1.getarea(1));
+
+            //Console.WriteLine("C1 Çevre değeri : {0}", c1.getarea());
+            //Console.WriteLine("C1 Alan değeri : {0}", c1.getPerimeter());
+            //Console.WriteLine("C2 Çevre değeri : {0}", c2.getarea());
+            //Console.WriteLine("C2 Alan değeri : {0}", c2.getPerimeter());
+
+        }
+    }
+}
